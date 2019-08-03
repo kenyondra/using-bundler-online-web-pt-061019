8f2b2667e2c9c488225b6ee6ec965a0a683f4402
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-08-03 14:03:44 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
-  # Run specs in random order to surface order dependencies. If you find an
+  # Run specs in random order to surface order dependencies. If you find anhttps://learn.co/tracks/online-software-engineering-structured/object-oriented-ruby/scraping/gems-and-bundler#
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
